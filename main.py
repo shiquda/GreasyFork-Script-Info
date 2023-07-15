066fd080cd54ba14f6960197667522b00b246b5f
shiquda <1847259360@qq.com>
2023-07-15 14:40:12 +0800

    update

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from tqdm import tqdm
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import tqdm
 import threading
 import requests
-import pandas as pd
 import argparse
-
+import csv
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('start_id', type=int)
@@ -16,110 +17,105 @@ start_id = args.start_id
 end_id = args.end_id
 num_threads = args.num_threads
 
-data_lock = threading.Lock()
+lock = threading.Lock()
 data = []
 failed_ids = []
 
 def getScriptInfo(id):
     url = "https://greasyfork.org/zh-CN/scripts/" + str(id)
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    script_info = soup.find("section", attrs={"id": "script-info"})
-
-    if script_info is None:
-        if "已被删除。" in response.text or "This script has been deleted" in response.text:
-            print(f"id:{id} 已被删除。")
-        elif response.text.find("404") != -1:
-            print(f"id:{id} 访问404")
-        else:
-            with data_lock:
-                failed_ids.append(id)
-    else:
-        name = None
-        description = None
-        author = None
-        site = None
-        license = None
-
-        try:
-            name = script_info.find("h2").string
-            description = script_info.find("p", attrs={"id": "script-description"}).string
-            author = script_info.find("dd", attrs={"class": "script-show-author"}).find("span").find("a").string
-            site = script_info.find("ul", attrs={"class": "block-list"}).find("li").find("a").string
-            license = script_info.find("dd", attrs={"class": "script-show-license"}).find("span").string
-        except AttributeError:
-            pass
-
-        # 创建包含数据的字典
-        script_data = {
-            "id": id,
-            "名称": name,
-            "描述": description,
-            "作者": author,
-            "URL": url,
-            "应用到": site,
-            "许可证": license
-        }
-
-        with data_lock:
-            data.append(script_data)
-
-def getAllScriptInfo(start_id, end_id, num_threads):
-    # 读取现有数据
     try:
-        existing_data = pd.read_csv("script_info.csv")
-        existing_ids = set(existing_data["id"].tolist())
-        start_id = max(start_id, max(existing_ids) + 1)
-    except FileNotFoundError:
-        existing_data = None
+        response = session.get(url)
+        response.raise_for_status()
 
-    threads = []
-    progress_bar = tqdm(total=end_id - start_id + 1, desc="进度")
+        soup = BeautifulSoup(response.text, "html.parser")
+        script_info = soup.find("section", attrs={"id": "script-info"})
 
-    for id in range(start_id, end_id + 1):
-        if existing_data is not None and id in existing_ids:
-            progress_bar.update(1)
-            continue
+        if script_info is None:
+            if "已被删除。" in response.text or "This script has been deleted" in response.text:
+                print(f"id:{id} 已被删除。")
+            elif response.text.find("404") != -1:
+                print(f"id:{id} 访问404")
+            else:
+                with lock:
+                    failed_ids.append(id)
+        else:
+            name = None
+            description = None
+            author = None
+            site = None
+            license = None
 
-        thread = threading.Thread(target=getScriptInfo, args=(id,))
-        threads.append(thread)
-        thread.start()
+            try:
+                name = script_info.find("h2").string
+                description = script_info.find("p", attrs={"id": "script-description"}).string
+                author = script_info.find("dd", attrs={"class": "script-show-author"}).find("span").find("a").string
+                site = script_info.find("ul", attrs={"class": "block-list"}).find("li").find("a").string
+                license = script_info.find("dd", attrs={"class": "script-show-license"}).find("span").string
+            except AttributeError:
+                pass
 
-        if len(threads) >= num_threads:
-            for thread in threads:
-                thread.join()
+            script_data = {
+                "id": id,
+                "名称": name,
+                "描述": description,
+                "作者": author,
+                "URL": url,
+                "应用到": site,
+                "许可证": license
+            }
 
-            threads = []
-            progress_bar.update(num_threads)
+            with lock:
+                data.append(script_data)
+    except Exception as e:
+        print(f"请求id:{id}时出错：{str(e)}")
 
-    for thread in threads:
-        thread.join()
-        progress_bar.update(1)
+def processBatch(start_id, end_id, batch_size):
+    futures = []
+    progress_bar = tqdm.tqdm(total=end_id - start_id + 1, desc="进度")
+
+    for id in range(start_id, end_id + 1, batch_size):
+        with ThreadPoolExecutor(max_workers=num_threads) as executor:
+            batch = range(id, min(id + batch_size, end_id + 1))
+            for id in batch:
+                future = executor.submit(getScriptInfo, id)
+                futures.append(future)
+
+            for future in as_completed(futures):
+                future.result()
+                progress_bar.update(1)
 
     progress_bar.close()
 
     if data:
-        # 将字典对象转换为 DataFrame
-        combined_df = pd.DataFrame(data)
+        with lock:
+            existing_data = []
+            try:
+                with open("script_info.csv", "r") as f:
+                    reader = csv.DictReader(f)
+                    existing_data = list(reader)
+            except FileNotFoundError:
+                existing_data = []
 
-        if existing_data is not None:
-            combined_df = pd.concat([existing_data, combined_df], ignore_index=True)
+            combined_data = existing_data + data
 
-        combined_df.to_csv("script_info.csv", index=False)
+            with open("script_info.csv", "w", newline="") as f:
+                writer = csv.DictWriter(f, fieldnames=combined_data[0].keys())
+                writer.writeheader()
+                writer.writerows(combined_data)
+
         print("数据保存至 script_info.csv")
 
     if failed_ids:
-        print("以下id请求失败：",failed_ids)
-    # 读取CSV文件
-    data_frame = pd.read_csv("script_info.csv")
-
-    # 按ID列进行升序排序
-    sorted_data = data_frame.sort_values("id")
-
-    # 将排序后的数据覆盖原始CSV文件
-    sorted_data.to_csv("script_info.csv", index=False)
+        print("以下id请求失败：", failed_ids)
 
     print("数据已按ID排序并保存至 script_info.csv 文件。")
 
+# 使用连接池来管理与远程服务器的连接
+session = requests.Session()
 
-getAllScriptInfo(start_id, end_id, num_threads)
+start_time = time.time()
+processBatch(start_id, end_id, 100)
+end_time = time.time()
+execution_time = end_time - start_time
+
+print(f"脚本已优化完成，总耗时: {execution_time}秒")
